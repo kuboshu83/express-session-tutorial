62392a8c9728c412380fb6c7a13b7ae19e87a6ef
kuboshu83 <dot.sohkubo@gmail.com>
2024-09-11 17:38:25 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express } from "express";
 import path from "path";
 import { User } from "./models/user";
 import mongoose, { ObjectId } from "mongoose";
@@ -15,58 +15,77 @@ declare module "express-session" {
   }
 }
 
-const redisClient = createClient();
-// デフォルトでローカルのポート6379に接続しに行く
-redisClient
-  .connect()
-  .then(() => console.log("redis connection is established ..."))
-  .catch((e) => console.log(`redis connection error: ${e.message}`));
+function setupMiddleware(app: Express) {
+  app.set("views", path.join(__dirname, "../views"));
+  app.set("view engine", "ejs");
+  app.use(express.urlencoded({ extended: true }));
+}
 
-const redisStore = new RedisStore({
-  client: redisClient,
-  prefix: "myapp",
-});
+function setupRoute(app: Express) {
+  app.use("/auth", authRouter);
+  app.use("/user", userRouter);
+  app.get("/", async (req, res) => {
+    const userId = req.session.userId;
+    const user = await User.findById(userId);
+    res.render("index", { user });
+  });
+}
 
-const app = express();
+async function setupStrage() {
+  try {
+    // TODO: 実験で適当な認証情報をベタ書きしている、本番では.envに記載する。
+    await mongoose.connect("mongodb://localhost:27017", {
+      user: "akira",
+      pass: "akira",
+      dbName: "todo",
+    });
+    console.log("mongodb connection is established");
+  } catch (e: any) {
+    console.log(`mongodb connection error: ${e.message}`);
+    throw e;
+  }
+}
 
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "ejs");
+async function setupSession(app: Express): Promise<void> {
+  const redisClient = createClient();
+  const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: "myapp",
+  });
 
-app.use(express.urlencoded({ extended: true }));
-// TODO: 実験で適当な秘密鍵をベタ書きしている、本番では.envに記載する。
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "secret",
-    store: redisStore,
-  })
-);
+  try {
+    await redisClient.connect();
+    console.log("redis connection is established ...");
+  } catch (e: any) {
+    console.log(`redis connection error: ${e.message}`);
+    throw e;
+  }
 
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: "secret",
+      store: redisStore,
+    })
+  );
+}
 
-app.get("/", async (req, res) => {
-  const userId = req.session.userId;
-  const user = await User.findById(userId);
-  res.render("index", { user });
-});
-
-// TODO: 実験で適当なpassとuserをベタ書きしている、本番では.envに記載する
-mongoose
-  .connect("mongodb://localhost:27017", {
-    user: "akira",
-    pass: "akira",
-    dbName: "todo",
-  })
-  .then(() => {
-    console.log("mongodb connection is established ...");
-  })
-  .then(() => {
+async function main() {
+  try {
+    const app = express();
+    await setupSession(app);
+    setupMiddleware(app);
+    setupRoute(app);
+    await setupStrage();
     app.listen(3000, () => {
       console.log("server running ...");
     });
-  })
-  .catch((e) => {
-    console.log(`mongodb connection error: ${e.message}`);
-  });
+  } catch {
+    console.log("setup storage error");
+    console.log("stop application");
+    return;
+  }
+}
+
+main();
