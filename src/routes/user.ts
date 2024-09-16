561c4409d1f546ada5d17e68b41a56d165fcb19d
kuboshu83 <dot.sohkubo@gmail.com>
2024-09-16 15:49:43 +0900

    バリデーションを実行して、そのエラーハンドリング追加

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,11 +6,24 @@ const router = express.Router();
 // base url is /users
 router.post("/new", async (req, res) => {
   const { name, password } = req.body;
-  if (!name || !password) {
-    return res.status(400);
-  }
+
   const user = new User({ name, password });
-  const result = await user.save();
+  try {
+    // mongooseはデフォルトではsaveの直前にしかvalidateしない。
+    // なので、ここで手動で実行している。
+    await user.validate();
+  } catch (e: any) {
+    console.log(e.message);
+    return res.status(400).send(`invalid name or password`);
+  }
+
+  let result;
+  try {
+    result = await user.save();
+  } catch (e: any) {
+    console.log(e.message);
+    return res.status(500).send(`internal server error`);
+  }
   console.log(`new user ${result.name} is created`);
   req.session.userId = result._id.toString();
   res.redirect("/");
