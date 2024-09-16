41e972077970374506a4563ebfac66320b191f58
kuboshu83 <dot.sohkubo@gmail.com>
2024-09-16 15:31:32 +0900

    ユーザ名とパスワードにバリデーション追加

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  password: String,
+interface IUser {
+  name: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 10,
+  },
 });
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model<IUser>("User", userSchema);
